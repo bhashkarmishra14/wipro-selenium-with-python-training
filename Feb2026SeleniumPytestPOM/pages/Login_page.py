00617bb7bf60e684ff09e69849cae2a60935268f
bhashkarmishra14 <114055761+bhashkarmishra14@users.noreply.github.com>
2026-02-23 09:56:03 +0530

    Add files via upload

---
+++ Feb2026SeleniumPytestPOM/pages/Login_page.py
@@ -0,0 +1,23 @@
+'''
+'''
+'''
+locators = Page actions
+
+reusability ,
+easy maintainece
+readability
+
+
+logon page
+1. verify login with valid credentials
+2. verify login with invalid credentials
+3.Verify password masking
+4.Verify with blank credentials
+5.Verify input text field validation
+6 Verify password field allowance
+7. verify logo of the login page
+'''
+'''
+'''
+
+
